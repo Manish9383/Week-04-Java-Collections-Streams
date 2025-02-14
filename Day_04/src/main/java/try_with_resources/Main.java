a1428fac9e5e84a4eda88b34108d9ac7529753e8
Manish <patelmanish131149198@gmail.com>
2025-02-14 16:38:16 +0530

    [Manish]Add. Try with resources code

---
+++ Day_04/src/main/java/try_with_resources/Main.java
@@ -0,0 +1,8 @@
+package try_with_resources;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath="src/main/resources/info.txt";
+        TryWithResourcesDemo.generateException(filePath);
+    }
+}
