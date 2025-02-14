33fc3197c7840de41c69e3e9f1071a40f0574a68
Manish <patelmanish131149198@gmail.com>
2025-02-14 16:34:55 +0530

    [Manish]Add. Bank transaction system code

---
+++ Day_04/src/main/java/bank_transaction_system/Main.java
@@ -0,0 +1,14 @@
+package bank_transaction_system;
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(1000);
+        try {
+            account.withdraw(500);
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
