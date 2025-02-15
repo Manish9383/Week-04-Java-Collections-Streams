37da0202e77196502eb8cfd87163bcb395282321
Manish <patelmanish131149198@gmail.com>
2025-02-15 21:55:32 +0530

    [Manish]Add. Testing parameterized tests code

---
+++ Day_05/src/main/java/junit/testing_parameterized_tests/NumberUtils.java
@@ -0,0 +1,10 @@
+package junit.testing_parameterized_tests;
+
+
+public class NumberUtils {
+
+    // Checks if a number is even or not
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
