eae58d624ef082fb82fff7483c451750aa67b1d6
Manish <patelmanish131149198@gmail.com>
2025-02-16 16:27:50 +0530

    [Manish]Add. Program for create a todo annotaion for pending tasks

---
+++ Day_06/src/main/java/problems_on_annotations_in_java/practice_problems_for_custom_annotations_in_java/beginner_level/create_a_todo_annotation_for_pending_tasks/Priority.java
@@ -0,0 +1,6 @@
+package problems_on_annotations_in_java.practice_problems_for_custom_annotations_in_java.beginner_level.create_a_todo_annotation_for_pending_tasks;
+
+
+public enum Priority {
+    LOW, MEDIUM, HIGH;
+}
